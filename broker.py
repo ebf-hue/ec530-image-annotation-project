b41f8c0488f3dbbb640970de456bde54f79b9ac5
ebf-hue <129566264+ebf-hue@users.noreply.github.com>
2026-04-12 17:34:18 -0400

    create broker.py pub sub functions

---
+++ broker.py
@@ -0,0 +1,39 @@
+# broker.py
+
+import json
+import redit
+from events import validate_event
+
+# broker will act as the middleman between the events.py schemas and the actual Redis data
+class Broker:
+    def __init__(self):
+        # to pub, localhost on port 5678
+        self.client = redis.Redis(host = "localhost", port = 5678, decode_responses=True)
+        # to sub
+        self.pubsub = self.client.pubsub()
+
+    def publish(self, event: dict):
+        # validate event
+        if not validate_event(event):
+            print(f"[oops] bad event: {event}")
+            return
+        topic = event["topic"]
+        # json.dumps converts to json string
+        self.client.publish(topic, json.dumps(event))
+
+    # subscribe
+    def subscribe(self, topic: str, handler):
+        self.pubsub.subscribe(**{topic: lambda msg: self._handle(msg, handler)})
+
+    # handle a msg
+    def _handle(self, msg, handler):
+        try:
+            event = json.loads(msg["data"])
+            handler(event)
+        except Exception as e:
+            print(f"[oops] error handling msg: {e}")
+
+    # listen
+    def listen(self):
+        self.pubsub.run_forever()
+        
