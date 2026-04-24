8e957184b50be9bc8288fa787331fa8afdd42200
ebf-hue <129566264+ebf-hue@users.noreply.github.com>
2026-04-24 14:39:19 -0400

    fix typo in upload_service.py

--- services/upload_service.py
+++ services/upload_service.py
@@ -4,6 +4,6 @@
 from events import image_submitted
 
 #publishes: image.submitted
-def handle_cli_image(broker, image_id: str, path: str, source: str):
-    event = image_submitted(image_id=image_id, path=path, source=source)
+def handle_cli_image(broker, image_id: str, path: str):
+    event = image_submitted(image_id=image_id, path=path)
     return broker.publish(event)
